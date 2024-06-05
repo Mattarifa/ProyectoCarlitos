2d2b7d5655e4f0b2e9278614a07aa8f7fe4ebb67
carlos tananta <65621751+steyker4@users.noreply.github.com>
2024-06-05 00:07:30 -0300

    Update main.c
    añadi las funciones que hicimos, llegamos hasta modificar un auto, pero creo que no lo habiamos terminado

--- main.c
+++ main.c
@@ -6,6 +6,60 @@
 
 #define LONGITUD 20
 
+/*typedef struct {        //patente
+	char letras[3];
+	int numeros;
+
+} Patente;
+
+typedef struct {        //autoArchivo
+	Patente patente;
+	char marca[30];
+	char modelo[30];
+	int anio;
+	int kms;
+	int dniTitular;
+	float precioDeAdquisicion;
+
+} AutoArchivo;
+
+typedef struct {        //persona
+	int dni;
+	char nombre[40];
+	int telefono;
+	char direccion[30];
+	char rol[10]; //comprador o vendedor
+
+} Persona;
+
+typedef struct {        //auto
+	Patente patente;
+	char marca[30];
+	char modelo[30];
+	int anio;
+	int kms;
+	Persona Titular;
+	float precioDeAdquisicion;
+
+} Auto;
+
+typedef struct {        //fecha
+	int dia;
+	int mes;
+	int anio;
+
+} Fecha;
+
+typedef struct {        //venta
+	Fecha fecha;
+	Patente autoAVender;
+	float precioVenta;
+	float ganancia;
+	int dniComprador;
+	int dniVendedor;
+
+} Venta;*/
+
 int main()
 {
 
@@ -73,3 +127,211 @@ void login(char *usuario, char *clave, int *intento, int *ingresa, char caracter
     }
     }while (*intento <3 && *ingresa == 0);
 }
+
+/*void agregarAutoArchivo()
+{
+    AutoArchivo autoArch;
+    char c='s';
+    FILE* archivo = fopen("autosArch","wb");
+    if(archivo!=NULL)
+    {
+        while(c=='s')
+        {
+            printf("ingrese las letras de la patente\n");
+            fflush(stdin);
+            scanf("%s",&autoArch.patente.letras);
+            printf("ingrese los numeros de la patente\n");
+            fflush(stdin);
+            scanf("%d",&autoArch.patente.numeros);
+
+
+            printf("ingrese la marca del auto\n");
+            fflush(stdin);
+            scanf("%s",&autoArch.marca);
+
+            printf("ingrese la modelo del auto\n");
+            fflush(stdin);
+            scanf("%s",&autoArch.modelo);
+
+            printf("ingrese el año del auto\n");
+            scanf("%d",&autoArch.anio);
+
+            printf("ingrese el kilometraje del auto\n");
+            scanf("%d",&autoArch.kms);
+
+            printf("ingrese el dni del titular del auto\n");
+            scanf("%d",&autoArch.dniTitular);
+
+            printf("ingrese el precio de adquisicion del auto\n");
+            scanf("%f",&autoArch.precioDeAdquisicion);
+
+            printf("desea continuar?(s/n)\n");
+            fflush(stdin);
+            scanf("%c",&c);
+
+            fwrite(&autoArch,sizeof(AutoArchivo),1,archivo);
+        }
+
+        fclose(archivo);
+    }
+    else
+    {
+        printf("error no se pudo abrir el catalogo");
+    }
+}
+void agregarPersonas()
+{
+    Persona p;
+    char c='s';
+    FILE* archivo = fopen("personas","wb");
+    if(archivo!=NULL)
+    {
+        while(c=='s')
+        {
+            printf("ingrese el dni\n");
+            fflush(stdin);
+            scanf("%d",&p.dni);
+            printf("ingrese el nombre\n");
+            fflush(stdin);
+            scanf("%s",&p.nombre);
+
+            printf("ingrese telefono\n");
+            fflush(stdin);
+            scanf("%d",&p.telefono);
+
+            printf("ingrese la direccion de la persona\n");
+            fflush(stdin);
+            scanf("%s",&p.direccion);
+
+            printf("ingrese el rol\n");
+            scanf("%s",&p.rol);
+
+            printf("desea continuar?(s/n)\n");
+            fflush(stdin);
+            scanf("%c",&c);
+
+            fwrite(&p,sizeof(Persona),1,archivo);
+        }
+
+        fclose(archivo);
+    }
+    else
+    {
+        printf("error no se pudo abrir el catalogo");
+    }
+}
+
+void mostrarAutoArchivo()
+{
+    AutoArchivo autoArch;
+    FILE* archivo = fopen("autosArch", "rb");
+
+    if(archivo!=NULL)
+    {
+        while(fread(&autoArch, sizeof(AutoArchivo), 1, archivo) > 0)
+        {
+            printf("Patente:    %s-%d\n", autoArch.patente.letras, autoArch.patente.numeros);
+            printf("Marca:      %s\n", autoArch.marca);
+            printf("Modelo:     %s\n", autoArch.modelo);
+            printf("Anio:       %d\n", autoArch.anio);
+            printf("Kms:        %d\n", autoArch.kms);
+            printf("DniTitular: %d\n", autoArch.dniTitular);
+            printf("PrecioAdq:  %.2f\n", autoArch.precioDeAdquisicion);
+        }
+        fclose(archivo);
+    } else
+    {
+        printf("error");
+    }
+}
+
+void mostrarPersonas()
+{
+    Persona p;
+    FILE* archivo = fopen("personas", "rb");
+
+    if(archivo!=NULL)
+    {
+        while(fread(&p, sizeof(Persona), 1, archivo) > 0)
+        {
+            printf("Dni:    \t%d\n", p.dni);
+            printf("Nombre:      \t%s\n", p.nombre);
+            printf("Telefono:     \t%d\n", p.telefono);
+            printf("Direccion:\t%s\n", p.direccion);
+            printf("Rol:\t\t%s\n", p.rol);
+
+        }
+        fclose(archivo);
+    } else
+    {
+        printf("error");
+    }
+}
+
+void agregarVentas()
+{
+    Venta v;
+    char c='s';
+    FILE* archivo = fopen("ventas","wb");
+    if(archivo!=NULL)
+    {
+        while(c=='s')
+        {
+            printf("ingrese dia, mes y anio de la venta:\n");
+            scanf("%d %d %d",&v.fecha.dia, &v.fecha.mes, &v.fecha.anio);
+
+            printf("ingrese las letras de la patente del auto a vender\n");
+            fflush(stdin);
+            scanf("%s",&v.autoAVender.letras);
+            printf("ingrese los numeros de la patente del auto a vender\n");
+            scanf("%d",&v.autoAVender.numeros);
+
+            printf("ingrese el precio de la venta\n");
+            scanf("%f",&v.precioVenta);
+
+            printf("ingrese la ganancia de la venta\n");
+            scanf("%f",&v.ganancia);
+
+            printf("ingrese el dni del comprador\n");
+            scanf("%d",&v.dniComprador);
+
+            printf("ingrese el dni del vendedor\n");
+            scanf("%d",&v.dniVendedor);
+
+            printf("desea continuar?(s/n)\n");
+            fflush(stdin);
+            scanf("%c",&c);
+
+            fwrite(&v,sizeof(Venta),1,archivo);
+        }
+
+        fclose(archivo);
+    }
+    else
+    {
+        printf("error no se pudo abrir el catalogo");
+    }
+}
+
+void mostrarVentas()
+{
+    Venta v;
+    FILE* archivo = fopen("ventas", "rb");
+
+    if(archivo!=NULL)
+    {
+        while(fread(&v, sizeof(Venta), 1, archivo) > 0)
+        {
+            printf("Fecha:        %d/%d/%d\n", v.fecha.dia, v.fecha.mes, v.fecha.anio);
+            printf("Patente:      %s-%d\n", v.autoAVender.letras, v.autoAVender.numeros);
+            printf("PrecioVenta:  %.2f\n", v.precioVenta);
+            printf("Ganancia:     %.2f\n", v.ganancia);
+            printf("DniComprador: %d\n", v.dniComprador);
+            printf("DniVendedor:  %d\n", v.dniVendedor);
+        }
+        fclose(archivo);
+    } else
+    {
+        printf("error");
+    }
+}*/
